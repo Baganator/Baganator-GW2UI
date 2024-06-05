cb3a5b2511c5fdbf4e92cb0e637654e02bfac26a
plusmouse <plusmouse@protonmail.com>
2024-06-05 20:46:08 +0100

    Redesign skin to fit better with other GW2 panels

--- Main.lua
+++ Main.lua
@@ -3,6 +3,34 @@ local GW = GW2_ADDON
 
 hooksecurefunc("SetItemButtonQuality", GW.SetBagItemButtonQualitySkin)
 
+local function ConvertTags(tags)
+  local res = {}
+  for _, tag in ipairs(tags) do
+    res[tag] = true
+  end
+  return res
+end
+
+Baganator.Constants.ButtonFrameOffset = 0
+
+local function SkinContainerFrame(frame)
+  frame:GwStripTextures()
+  GW.CreateFrameHeaderWithBody(frame, frame:GetTitleText(), "Interface/AddOns/GW2_UI/textures/bag/bagicon", {})
+  frame.gwHeader:ClearAllPoints()
+  frame.gwHeader:SetPoint("BOTTOMLEFT", frame, "TOPLEFT", 0, -25)
+  frame.gwHeader:SetPoint("BOTTOMRIGHT", frame, "TOPRIGHT", 0, -25)
+  frame.gwHeader.windowIcon:ClearAllPoints()
+  frame.gwHeader.windowIcon:SetPoint("CENTER", frame.gwHeader.BGLEFT, "LEFT", 21, -7)
+  frame.CloseButton:GwSkinButton(true)
+  frame.CloseButton:SetPoint("TOPRIGHT", 0, 7)
+  hooksecurefunc(frame.SearchWidget, "SetSpacing", function(_, sideSpacing)
+    frame.SearchWidget:ClearAllPoints()
+    frame.SearchWidget.SearchBox:SetPoint("RIGHT", frame, -sideSpacing - 36, 0)
+    frame.SearchWidget.SearchBox:SetPoint("TOPLEFT", frame, "TOPLEFT", Baganator.Constants.ButtonFrameOffset + 64, - 28)
+  end)
+  frame.SearchWidget.SearchBox:SetHeight(22)
+end
+
 local skinners = {
   ItemButton = function(frame)
     GW.SkinBagItemButton(frame:GetName(), frame, 37)
@@ -29,8 +57,29 @@ local skinners = {
   Button = function(frame)
     frame:GwSkinButton(false, true, false, false, false, false)
   end,
-  ButtonFrame = function(frame)
-    GW.HandlePortraitFrame(frame, true)
+  ButtonFrame = function(frame, tags)
+    frame:SetFrameStrata("HIGH")
+    if tags.backpack then
+      SkinContainerFrame(frame)
+      frame.TopButtons[1]:ClearAllPoints()
+      frame.TopButtons[1]:SetPoint("TOPLEFT", frame:GetTitleText(), "TOPRIGHT", 10, 0)
+      frame.BagSlots:ClearAllPoints()
+      frame.BagSlots:SetPoint("BOTTOM", frame, "TOP", 0, 8)
+      frame.BagSlots:SetPoint("LEFT", frame.TopButtons[1])
+    elseif tags.bank then
+      SkinContainerFrame(frame)
+      frame.Character.TopButtons[1]:ClearAllPoints()
+      frame.Character.TopButtons[1]:SetPoint("TOPLEFT", frame:GetTitleText(), "TOPRIGHT", 10, 0)
+      frame.Character.BagSlots:ClearAllPoints()
+      frame.Character.BagSlots:SetPoint("BOTTOM", frame, "TOP", 0, 8)
+      frame.Character.BagSlots:SetPoint("LEFT", frame.Character.TopButtons[1], "RIGHT")
+    elseif tags.guild then
+      SkinContainerFrame(frame)
+      frame.LiveButtons[1]:ClearAllPoints()
+      frame.LiveButtons[1]:SetPoint("TOPLEFT", frame:GetTitleText(), "TOPRIGHT", 10, 0)
+    else
+      GW.HandlePortraitFrame(frame, true)
+    end
   end,
   SearchBox = function(frame)
     frame:SetFont(UNIT_NAME_FONT, 14, "")
@@ -78,7 +127,7 @@ local skinners = {
 local function SkinFrame(details)
   local func = skinners[details.regionType]
   if func then
-    func(details.region)
+    func(details.region, details.tags and #details.tags > 0 and ConvertTags(details.tags) or {})
   end
 end
 
@@ -86,6 +135,8 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:SetScript("OnEvent", function()
   Baganator.API.Skins.RegisterListener(SkinFrame)
+
+  Baganator.Config.Set(Baganator.Config.Options.EMPTY_SLOT_BACKGROUND, true)
 
   for _, details in ipairs(Baganator.API.Skins.GetAllFrames()) do
     SkinFrame(details)
